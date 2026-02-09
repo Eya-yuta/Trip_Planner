5fe9b2b0ec2492a2563c5608546d1502236f40f5
Eya <benhasseneya28@gmail.com>
2026-02-09 08:08:12 +0100

    Fullstack project setup (Backend,Frontend and Git/Github files)

---
+++ backend/src/test/java/triply/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package triply.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
